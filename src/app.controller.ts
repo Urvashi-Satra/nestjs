bac8a8d7572eb6e6c76ef9c3e98bcbf8d3e82ccb
Urvashi <satraurvashi99@gmail.com>
2023-12-06 15:11:11 +0530

    app

---
+++ src/app.controller.ts
@@ -0,0 +1,13 @@
+import { Controller ,Module ,Get} from "@nestjs/common";
+@Controller("/app")
+export class AppController{
+    @Get()
+    getRootRoute(){
+        return "hi there";
+    }
+
+    @Get('/bye')
+    getByeThere(){
+        return 'bye there !';
+    }
+}
---
+++ src/app.module.ts
@@ -0,0 +1,8 @@
+import { Controller ,Module ,Get} from "@nestjs/common";
+import { AppController } from "./app.controller";
+@Module({
+    controllers:[AppController]
+})
+export class AppModule{
+
+}
